c1fd1f02ab3b616590fdb5f2beb06805cc4aadbb
Mic Thomerson <mic732@gmail.com>
2023-04-10 16:53:19 -0500

    Finished Project

---
+++ script.js
@@ -0,0 +1,44 @@
+const mcuShows = ["Loki", "Moon Knight"];
+const starWarsShows = ["The Mandalorian", "The Book of Boba Fett"];
+const disneyPlusShows = [...mcuShows, ...starWarsShows, "The Beatles: Get Back"]
+console.log(disneyPlusShows);
+const netflixMovies = {
+    action: 'Extraction',
+    crime: 'The Irishman', 
+};
+
+const amazonPrimeMovies = {
+    action: 'The Tommorrow War',
+    drama: 'One Night In Miami',
+};
+
+const streamingMovies = {
+    ...amazonPrimeMovies,
+    ...netflixMovies,
+    musical: 'Hamilton',
+};
+
+console.log(streamingMovies);
+
+const disneyJunior = ['Mickey Mouse Clubhouse', 'Spidey and His Amazing Friends'];
+const [mickey, spidey] = disneyJunior;
+console.log(disneyJunior);
+console.log(mickey, spidey);
+const avengers = {
+    warMachines: 'James Rhodes',
+    theHulk: 'Bruce Banner',
+}
+
+const {warMachine, theHulk} = avengers;
+
+console.log(warMachine, theHulk);
+
+const moreAvengers = {
+    blackWidow: 'Natasha Romanoff',
+    hawkeye: 'Clint Barton',
+    ironMan: 'Tony Stark',
+};
+
+const {blackWidow: nat, ...others} = moreAvengers;
+console.log(nat, others);
+
